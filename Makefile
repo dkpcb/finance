e23f304b21ddd4b93208cf203df5746425ece143
加藤大地 <katoudaichi@katoudaichinoMacBook-Air.local>
2024-07-20 23:07:25 +0900

    docker

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 # Docker Compose でサービスをビルドしてバックグラウンドで起動
+dev/run/import:
+	docker-compose up -d --build
+	docker-compose exec server go run main.go import
+
 dev/run/server:
 	docker-compose up -d --build
 	docker-compose exec server go run main.go
--- main.go
+++ main.go
@@ -1,6 +1,35 @@
+// package main
+
+// import (
+// 	"github.com/dkpcb/finance/data"
+// 	"github.com/dkpcb/finance/router"
+// 	_ "github.com/go-sql-driver/mysql"
+// 	"github.com/labstack/echo/v4"
+// )
+
+// func main() {
+
+// 	db, err := data.DBConnection()
+// 	if err != nil {
+// 		panic("failed to connect to database")
+// 	}
+
+// 	data.InitializeDatabase(db)
+
+// 	data.InsertCSVData("csv/trade_history.csv", "trade_history")
+// 	data.InsertCSVData("csv/reference_prices.csv", "reference_prices")
+
+// 	e := echo.New()
+// 	router.SetRouter(e, db)
+
+// }
+
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/dkpcb/finance/data"
 	"github.com/dkpcb/finance/router"
 	_ "github.com/go-sql-driver/mysql"
@@ -8,7 +37,14 @@ import (
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "import" {
+		runImport()
+	} else {
+		runServer()
+	}
+}
 
+func runImport() {
 	db, err := data.DBConnection()
 	if err != nil {
 		panic("failed to connect to database")
@@ -19,7 +55,17 @@ func main() {
 	data.InsertCSVData("csv/trade_history.csv", "trade_history")
 	data.InsertCSVData("csv/reference_prices.csv", "reference_prices")
 
+	fmt.Println("Data import completed")
+}
+
+func runServer() {
+	db, err := data.DBConnection()
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
 	e := echo.New()
 	router.SetRouter(e, db)
 
+	e.Logger.Fatal(e.Start(":8080"))
 }
