a7fb0fb66ecbf66cc303e6fc4b253ddbf4235687
加藤大地 <katoudaichi@katoudaichinoMacBook-Air.local>
2024-07-20 23:10:38 +0900

    main

--- main.go
+++ main.go
@@ -1,29 +1,3 @@
-// package main
-
-// import (
-// 	"github.com/dkpcb/finance/data"
-// 	"github.com/dkpcb/finance/router"
-// 	_ "github.com/go-sql-driver/mysql"
-// 	"github.com/labstack/echo/v4"
-// )
-
-// func main() {
-
-// 	db, err := data.DBConnection()
-// 	if err != nil {
-// 		panic("failed to connect to database")
-// 	}
-
-// 	data.InitializeDatabase(db)
-
-// 	data.InsertCSVData("csv/trade_history.csv", "trade_history")
-// 	data.InsertCSVData("csv/reference_prices.csv", "reference_prices")
-
-// 	e := echo.New()
-// 	router.SetRouter(e, db)
-
-// }
-
 package main
 
 import (
